9e9c7f015d28ff1462b801a7e1e48a85b5760d5f
Visitha Wickramasinghe <wickramasinghevisitha@gmail.com>
2026-01-08 21:52:10 +0530

    feat: added user service and repository

--- userservice/src/main/java/com/fitness/userservice/repository/UserRepository.java
+++ userservice/src/main/java/com/fitness/userservice/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.fitness.userservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+//Repository for user
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     boolean existsByEmail(String email);
