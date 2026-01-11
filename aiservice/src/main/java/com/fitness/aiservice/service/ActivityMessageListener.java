a5c4bb989347fbda947fbf7cb773fb1a96961e8a
Visitha Wickramasinghe <wickramasinghevisitha@gmail.com>
2026-01-11 13:12:20 +0530

    feat: configured rabbitmq

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+// RabbitMQ listener
 @Service
 @Slf4j
 @RequiredArgsConstructor
